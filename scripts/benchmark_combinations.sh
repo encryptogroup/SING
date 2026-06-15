716a3d5b5474d7345c87d41839ffe23442ac816f
joachimschmidt557 <joachim.schmidt557@outlook.com>
2026-06-15 17:18:22 +0200

    scripts/benchmark_combinations.sh: add default arguments

--- scripts/benchmark_combinations.sh
+++ scripts/benchmark_combinations.sh
@@ -2,12 +2,24 @@
 
 set -e
 
-checkpoints=$1
-network_settings=$2
+SING1=pretrained/share_assignment_supervised.pt
+SING2=pretrained/share_assignment_semi_supervised_silph.pt
+SING3=pretrained/share_assignment_semi_supervised_runtime.pt
+
+checkpoints=${1:-$SING1,$SING2,$SING3}
+network_settings=${2:-LAN,WAN}
 
 IFS=","
 for checkpoint in $checkpoints; do
     for network_setting in $network_settings; do
-        python benchmark_mpc.py --mode benchmark --hashes-file paper_benchmark_hashes.txt --checkpoint "$checkpoint" --parallel --max-jobs 4 --network-setting "$network_setting" --timeout 120
+        python benchmark_mpc.py \
+               --mode benchmark \
+               --benchmark-silph \
+               --hashes-file paper_benchmark_hashes.txt \
+               --checkpoint "$checkpoint" \
+               --parallel \
+               --max-jobs 4 \
+               --network-setting "$network_setting" \
+               --timeout 120
     done
 done
